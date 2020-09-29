b09f8ca5146acbfaab922ec566e15f58fcb19f8d
roinissan <roinissan12@gmail.com>
2020-09-29 20:50:49 +0300

    remove web

--- Web/Node-Vue/Recipes Site/Frontend/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  configureWebpack: {
-    devtool: "source-map"
-  },
-  devServer: {
-    host: "localhost"
-  }
-  // publicPath: process.env.NODE_ENV === "production" ? "/LAB12/" : "/"
-};
--- Web/Node-Vue/Recipes Site/README.md
+++
@@ -1,6 +0,0 @@
-API - https://app.swaggerhub.com/apis/roinis/RecipeSite/1.0.0
-
-Creators :
-
-Roei Cohen
-Roei Nissan
